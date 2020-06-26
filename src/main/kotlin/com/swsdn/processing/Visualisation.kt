925b1dd7b40845bdbc3825c2fd9c4f84809f3bbb
Krzysiek Kolatorowicz <kkolatorowicz@efigence.com>
2020-06-26 10:33:18 +0200

    Fix boundaries

--- src/main/kotlin/com/swsdn/processing/Visualisation.kt
+++ src/main/kotlin/com/swsdn/processing/Visualisation.kt
@@ -3,14 +3,15 @@ package com.swsdn.processing
 import processing.core.PApplet
 import processing.core.PConstants
 import java.util.*
+import kotlin.collections.ArrayList
 import kotlin.concurrent.schedule
 import kotlin.random.Random
 
 class Visualisation : PApplet() {
 
     companion object {
-        const val CELL_SIZE = 5
-        const val SCREEN_X = 1400
+        const val CELL_SIZE = 10
+        const val SCREEN_X = 1440
         const val SCREEN_Y = 900
         const val GRID_X = SCREEN_X / CELL_SIZE
         const val GRID_Y = SCREEN_Y / CELL_SIZE
@@ -20,20 +21,20 @@ class Visualisation : PApplet() {
         }
     }
 
-    var threshold = 10
+    var threshold = 20
 
     data class Grid(
             val sizeX: Int,
             val sizeY: Int,
             val threshold: Int,
-            val cells: Array<Array<Boolean>> = Array(sizeX) { Array(sizeY) { Random.nextInt(50) < threshold } }
+            val cells:List<List<Boolean>> = List(sizeX) {List(sizeY) { Random.nextInt(50) < threshold }}
     ) {
 
         private fun drawCell(x: Int, y: Int, draw: (x: Float, y: Float, size: Float) -> Unit) {
             if (cells[x][y]) {
                 val xf = x.toFloat() * CELL_SIZE
                 val yf = y.toFloat() * CELL_SIZE
-                draw.invoke(xf, yf, CELL_SIZE.toFloat())
+                draw.invoke(xf, yf, (CELL_SIZE - 3).toFloat())
             }
         }
 
@@ -46,28 +47,28 @@ class Visualisation : PApplet() {
         }
 
         fun evolve(): Grid {
-            val cells = cells.copyOf()
+            val newCells: MutableList<MutableList<Boolean>> = MutableList(sizeX) { MutableList(sizeY) { false } }
             for (x in 0 until sizeX) {
-                cells[x] = this.cells[x].copyOf()
+                newCells[x] = cells[x].toMutableList()
                 for (y in 0 until sizeY) {
                     val neighbours = getNeighboursCount(x, y)
-                    if (cells[x][y] && (neighbours < 2 || neighbours > 3)) {
-                        cells[x][y] = false
-                    } else if (!cells[x][y] && neighbours == 3) {
-                        cells[x][y] = true
+                    if (neighbours < 2 || neighbours > 3) {
+                        newCells[x][y] = false
+                    } else if (neighbours == 3) {
+                        newCells[x][y] = true
                     }
                 }
             }
-            return Grid(sizeX, sizeY, threshold, cells)
+            return this.copy(cells = newCells)
         }
 
         private fun getNeighboursCount(x: Int, y: Int): Int {
             var count = 0
             for (i in -1..1) {
                 for (j in -1..1) {
-                    val xIdx = x + i
-                    val yIdx = y + j
-                    if (xIdx == yIdx || xIdx < 0 || xIdx >= sizeX || yIdx < 0 || yIdx >= sizeY) continue
+                    val xIdx = (x + i + sizeX) % sizeX
+                    val yIdx = (y + j + sizeY) % sizeY
+                    if (i == j) continue
                     if (cells[xIdx][yIdx]) {
                         count++
                     }
@@ -75,27 +76,6 @@ class Visualisation : PApplet() {
             }
             return count
         }
-
-        override fun equals(other: Any?): Boolean {
-            if (this === other) return true
-            if (javaClass != other?.javaClass) return false
-
-            other as Grid
-
-            if (sizeX != other.sizeX) return false
-            if (sizeY != other.sizeY) return false
-            if (!cells.contentDeepEquals(other.cells)) return false
-
-            return true
-        }
-
-        override fun hashCode(): Int {
-            var result = sizeX
-            result = 31 * result + sizeY
-            result = 31 * result + cells.contentDeepHashCode()
-            return result
-        }
-
     }
 
     private var grid = Grid(GRID_X, GRID_Y, threshold)
@@ -121,7 +101,7 @@ class Visualisation : PApplet() {
         } else if (keyCode == 82) {
             grid = Grid(GRID_X, GRID_Y, threshold)
         } else if (keyCode == PConstants.UP) {
-            threshold = (++threshold).coerceAtMost(25)
+            threshold = (++threshold).coerceAtMost(50)
             println("threshold: $threshold")
         } else if (keyCode == PConstants.DOWN) {
             threshold = (--threshold).coerceAtLeast(0)
